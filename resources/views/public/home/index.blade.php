3bf3f06e44bd5b4dd99f78b1d7818cb945442d10
Ela Nadila <elanadila15@gmail.com>
2021-09-05 10:44:57 +0700

    category

--- resources/views/public/home/index.blade.php
+++ resources/views/public/home/index.blade.php
@@ -3,6 +3,67 @@
 @section('container')
 <!DOCTYPE html>
 <html lang="en">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+
+}
+
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 2.5s;
+  animation-name: fade;
+  animation-duration: 2.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
 
   <body>
     <!-- Page Content -->
@@ -11,42 +72,32 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-12" data-aos="zoom-in">
-              <div
-                id="storeCarousel"
-                class="carousel slide"
-                data-ride="carousel"
-              >
-                <ol class="carousel-indicators">
-                  <li
-                    data-target="#storeCarousel"
-                    data-slide-to="0"
-                    class="active"
-                  ></li>
-                  <li data-target="#storeCarousel" data-slide-to="1"></li>
-                  <li data-target="#storeCarousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-				  <div class="slider">
-				  <figure>
-					<div class="slide">
-                    <img
-                      src="{{ ('assets/img/banner1.jpg') }}"
-                      class="d-block w-100"
-                      alt="Carousel Image"
-                    />
-					</div>
-                  <div class="slide">
-                    <img
-                      src="{{ ('assets/img/banner2.jpg') }}"
-                      class="d-block w-100"
-                      alt="Carousel Image"
-                    />
-                  </div>
-				  </figure>
-				  </div>
-                </div>
-              </div>
+              
+                <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 1</div>
+  <img src="{{ ('assets/img/farming.jpg') }}" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="{{ ('assets/img/peternakan.jpg') }}" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="{{ ('assets/img/buah.jpg') }}" style="width:100%">
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
             </div>
           </div>
         </div>
@@ -67,13 +118,13 @@
               <a class="component-categories d-block" href="{{ route('category.detail', 1) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-baby.svg') }}"
+                    src="{{ ('assets/img/agronomy.png') }}"
                     alt="Gadgets Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                  UKM Pertanian
+                  Agriculture
                 </p>
               </a>
             </div>
@@ -85,13 +136,13 @@
               <a class="component-categories d-block" href="{{ route('category.detail', 2) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-gadgets.svg') }}"
+                    src="{{ ('assets/img/fast-food.png') }}"
                     alt="Furniture Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                UKM Pangan
+                Food
                 </p>
               </a>
             </div>
@@ -103,13 +154,13 @@
               <a class="component-categories d-block" href="{{ route('category.detail', 3) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-makeup.svg') }}"
+                    src="{{ ('assets/img/souvenir.png') }}"
                     alt="Makeup Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                UKM Souvenir
+                Souvenir
                 </p>
               </a>
             </div>
@@ -121,13 +172,13 @@
               <a class="component-categories d-block" href="{{ route('category.detail', 4) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-sneaker.svg') }}"
+                    src="{{ ('assets/img/cow.png') }}"
                     alt="Sneaker Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                UKM Peternakan
+                Farm
                 </p>
               </a>
             </div>
@@ -136,16 +187,16 @@
               data-aos="fade-up"
               data-aos-delay="400"
             >
-              <a class="component-categories d-block" href="{{ route('category.detail', 4) }}">
+              <a class="component-categories d-block" href="{{ route('category.detail', 5) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-tools.svg') }}"
+                    src="{{ ('assets/img/origami.png') }}"
                     alt="Sneaker Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                UKM Kerajinan
+                Craft
                 </p>
               </a>
             </div>
@@ -154,16 +205,16 @@
               data-aos="fade-up"
               data-aos-delay="400"
             >
-              <a class="component-categories d-block" href="{{ route('category.detail', 4) }}">
+              <a class="component-categories d-block" href="{{ route('category.detail', 6) }}">
                 <div class="categories-image">
                   <img
-                    src="{{ ('assets/img/categories-furniture.svg') }}"
+                    src="{{ ('assets/img/paint-palette.png') }}"
                     alt="Sneaker Categories"
                     class="w-100"
                   />
                 </div>
                 <p class="categories-text">
-                UKM Kesenian
+                Art
                 </p>
               </a>
             </div>
@@ -211,4 +262,26 @@
     </div>
   </body>
 </html>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
 @endsection
